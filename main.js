f4e954dc8613c971e6d4209766ef7a014af0d414
Kupz <benjohnvillabesa@gmail.com>
2023-06-03 11:10:28 +0800

    update sms modal and signup modal

--- main.js
+++ main.js
@@ -6,4 +6,11 @@ btnLogin.onclick = () => {
   window.location.href = "./home.html";
 };
 
+const signupBtn = document.getElementById("signup-modal");
 
+function signUpTrigger() {
+  signupBtn.style.display = "flex";
+}
+function signUpTriggerClose() {
+  signupBtn.style.display = "none";
+}
